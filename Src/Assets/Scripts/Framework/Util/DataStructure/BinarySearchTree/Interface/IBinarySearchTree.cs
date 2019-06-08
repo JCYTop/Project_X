3316f68f3c36d5e882d520700771768a7888ef2a
JCYTop <jcyemail@qq.com>
2019-06-08 22:04:11 +0800

    删除部分数据结构

--- Src/Assets/Scripts/Framework/Util/DataStructure/BinarySearchTree/Interface/IBinarySearchTree.cs
+++
@@ -1,4 +0,0 @@
-﻿public interface IBinarySearchTree
-{
-    float SortScore { get; }
-}
--- Src/Assets/Scripts/Framework/Util/DataStructure/LinkedList/Interface/ISelfList.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ISelfList<T>
-{
-    void Accept(ISelfListVisitor<T> visitor);
-    void Accept(ListVisitorDelegate<T> visitor);
-}
