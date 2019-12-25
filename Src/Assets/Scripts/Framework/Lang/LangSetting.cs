b30f68255113d2f37199c8c4f9ca2d67c0fd0d51
JCYTop <jcyemail@qq.com>
2019-12-25 20:46:47 +0800

    添加命名规范给代码 删除Pool代码

--- Src/Assets/Scripts/Framework/Lang/LangSetting.cs
+++ Src/Assets/Scripts/Framework/Lang/LangSetting.cs
@@ -13,114 +13,117 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LangSetting : MonoBehaviour
+namespace Framework.Lang
 {
-    private static LangSetting instance;
-    private Dictionary<int, Hashtable> wordMap = new Dictionary<int, Hashtable>(); //语言索引表
-    private LanguageType currLanguage = LanguageType.ChineseSimplified;
-
-    #region Private
-
-    public static LangSetting Instance()
+    public class LangSetting : MonoBehaviour
     {
-        if (instance == null)
+        private static LangSetting instance;
+        private Dictionary<int, Hashtable> wordMap = new Dictionary<int, Hashtable>(); //语言索引表
+        private LanguageType currLanguage = LanguageType.ChineseSimplified;
+
+        #region Private
+
+        public static LangSetting Instance()
         {
-            instance = MonoSingletonProperty<LangSetting>.Instance();
-            instance.Init();
-        }
-
-        return instance;
-    }
-
-    void Awake()
-    {
-        Instance();
-    }
-
-    /// <summary>
-    /// 初始化操作
-    /// </summary>
-    private void Init()
-    {
-        GetCurLang();
-        ParseWord();
-    }
-
-    private void ParseWord()
-    {
-        if (ExcelDataMgr.Instance().Language != null)
-        {
-            var datas = ExcelDataMgr.Instance().Language.dataArray;
-            foreach (var item in datas)
+            if (instance == null)
             {
-                var tmp = new Hashtable();
-                tmp.Add(LanguageType.ChineseSimplified, item.Chinesesimplified);
-                tmp.Add(LanguageType.ChineseTraditional, item.Chinesetraditional);
-                tmp.Add(LanguageType.English, item.English);
-                wordMap.Add(item.Languagekey, tmp);
-            }
-        }
-    }
-
-    private LanguageType GetCurLang()
-    {
-        //TODO 更换存储方式
-        var language = PlayerPrefs.GetString("Language");
-        if (string.IsNullOrEmpty(language))
-        {
-            currLanguage = LanguageType.ChineseSimplified;
-            SetCurLang(currLanguage);
-        }
-        else
-        {
-            currLanguage = (LanguageType) Enum.Parse(typeof(LanguageType), language);
-        }
-
-        return currLanguage;
-    }
-
-    private void SetCurLang(LanguageType Language)
-    {
-        //TODO 更换存储方式
-        PlayerPrefs.SetString("Language", Enum.GetName(typeof(LanguageType), Language));
-    }
-
-    private string _GetWord(int key)
-    {
-        if (key != null)
-        {
-            var word = wordMap[key][currLanguage] as string;
-            if (string.IsNullOrEmpty(word))
-            {
-                word = key.ToString();
+                instance = MonoSingletonProperty<LangSetting>.Instance();
+                instance.Init();
             }
 
-            return word;
+            return instance;
         }
 
-        return string.Empty;
+        void Awake()
+        {
+            Instance();
+        }
+
+        /// <summary>
+        /// 初始化操作
+        /// </summary>
+        private void Init()
+        {
+            GetCurLang();
+            ParseWord();
+        }
+
+        private void ParseWord()
+        {
+            if (ExcelDataMgr.Instance().Language != null)
+            {
+                var datas = ExcelDataMgr.Instance().Language.dataArray;
+                foreach (var item in datas)
+                {
+                    var tmp = new Hashtable();
+                    tmp.Add(LanguageType.ChineseSimplified, item.Chinesesimplified);
+                    tmp.Add(LanguageType.ChineseTraditional, item.Chinesetraditional);
+                    tmp.Add(LanguageType.English, item.English);
+                    wordMap.Add(item.Languagekey, tmp);
+                }
+            }
+        }
+
+        private LanguageType GetCurLang()
+        {
+            //TODO 更换存储方式
+            var language = PlayerPrefs.GetString("Language");
+            if (string.IsNullOrEmpty(language))
+            {
+                currLanguage = LanguageType.ChineseSimplified;
+                SetCurLang(currLanguage);
+            }
+            else
+            {
+                currLanguage = (LanguageType) Enum.Parse(typeof(LanguageType), language);
+            }
+
+            return currLanguage;
+        }
+
+        private void SetCurLang(LanguageType Language)
+        {
+            //TODO 更换存储方式
+            PlayerPrefs.SetString("Language", Enum.GetName(typeof(LanguageType), Language));
+        }
+
+        private string _GetWord(int key)
+        {
+            if (key != null)
+            {
+                var word = wordMap[key][currLanguage] as string;
+                if (string.IsNullOrEmpty(word))
+                {
+                    word = key.ToString();
+                }
+
+                return word;
+            }
+
+            return string.Empty;
+        }
+
+        #endregion
+
+        #region Public 
+
+        /// <summary>
+        /// 主方法
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string GetWord(int key)
+        {
+            return instance._GetWord(key);
+        }
+
+        #endregion
     }
 
-    #endregion
-
-    #region Public 
-
-    /// <summary>
-    /// 主方法
-    /// </summary>
-    /// <param name="key"></param>
-    /// <returns></returns>
-    public string GetWord(int key)
+    public enum LanguageType
     {
-        return instance._GetWord(key);
+        ChineseSimplified,
+        ChineseTraditional,
+        English,
     }
-
-    #endregion
-}
-
-public enum LanguageType
-{
-    ChineseSimplified,
-    ChineseTraditional,
-    English,
 }
