e5a701c2555250893cca3083d18b31b38ce33752
JCYTop <jcyemail@qq.com>
2019-12-28 08:19:51 +0800

    add quickSort

--- Src/Assets/Scripts/Framework/Util/Algorithm/Sort/Sort.cs
+++ Src/Assets/Scripts/Framework/Util/Algorithm/Sort/Sort.cs
@@ -96,5 +96,163 @@ namespace UtilSet
                 a1 = a1 ^ a2;
             }
         }
+
+        /// <summary>
+        /// 快速排序,顺序遍历法
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        public static void QucikSort_Sequence(int[] nums, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+
+            int mid = _partition(nums, start, end);
+            QucikSort_Sequence(nums, start, mid - 1);
+            QucikSort_Sequence(nums, mid + 1, end);
+
+            int _partition(int[] _nums, int _start, int _end)
+            {
+                var ptr = _nums[_end];
+                var storeIndex = _start;
+                for (int i = _start; i < _end; i++)
+                {
+                    if (_nums[i] < ptr)
+                    {
+                        _swap(ref _nums[storeIndex], ref _nums[i]);
+                        storeIndex++;
+                    }
+                }
+
+                _swap(ref _nums[storeIndex], ref _nums[_end]);
+                return storeIndex;
+            }
+
+            void _swap(ref int a1, ref int a2)
+            {
+                a1 = a1 ^ a2;
+                a2 = a1 ^ a2;
+                a1 = a1 ^ a2;
+            }
+        }
+
+        /// <summary>
+        /// 快速排序,普通法
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        public static void QucikSort_Normal(int[] nums, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+
+            var ptr = nums[start];
+            var p = start;
+            var q = end;
+            while (true)
+            {
+                while (nums[p] < ptr) p++;
+                while (nums[q] > ptr) q++;
+                if (p >= q) break;
+                _swap(ref nums[p], ref nums[q]);
+                p++;
+                q--;
+            }
+
+            QucikSort_Normal(nums, start, p - 1);
+            QucikSort_Normal(nums, q + 1, end);
+
+            void _swap(ref int a1, ref int a2)
+            {
+                a1 = a1 ^ a2;
+                a2 = a1 ^ a2;
+                a1 = a1 ^ a2;
+            }
+        }
+
+        /// <summary>
+        /// 快速排序,三数取中法;https://www.cnblogs.com/chengxiao/p/6262208.htmlv
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        public static void QucikSort_Three(int[] nums, int start, int end)
+        {
+            if (start < end)
+            {
+                //获取枢纽值，并将其放在当前待处理序列末尾
+                _dealPivot();
+                var ptr = end - 1;
+                var p = start;
+                var q = end - 1;
+                while (true)
+                {
+                    while (nums[p] < nums[ptr])
+                    {
+                        p++;
+                    }
+
+                    while (nums[q] > nums[ptr] && q > p)
+                    {
+                        q--;
+                    }
+
+                    //中间找到进行值的处理 ,之后继续寻找
+                    if (p < q)
+                    {
+                        _swap(ref nums[p], ref nums[q]);
+                    }
+                    else
+                    {
+                        //否则本次循环完成结束
+                        break;
+                    }
+                }
+
+                if (p < ptr)
+                {
+                    _swap(ref nums[p], ref nums[ptr]);
+                }
+
+                QucikSort_Three(nums, start, p - 1);
+                QucikSort_Three(nums, p + 1, end);
+            }
+
+
+            void _dealPivot()
+            {
+                var mid = (start + end) / 2;
+                if (nums[start] > nums[mid])
+                {
+                    _swap(ref nums[start], ref nums[mid]);
+                }
+
+                if (nums[start] > nums[end])
+                {
+                    _swap(ref nums[start], ref nums[end]);
+                }
+
+                if (nums[mid] > nums[end])
+                {
+                    _swap(ref nums[mid], ref nums[end]);
+                }
+
+                //把中间数放到右边数第二个
+                _swap(ref nums[end - 1], ref nums[mid]);
+            }
+
+            void _swap(ref int a1, ref int a2)
+            {
+                a1 = a1 ^ a2;
+                a2 = a1 ^ a2;
+                a1 = a1 ^ a2;
+            }
+        }
     }
 }
