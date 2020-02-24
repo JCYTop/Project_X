df7d4b629779d5423c0c6dece3375ce641656331
JCYTop <jcyemail@qq.com>
2020-02-24 22:31:19 +0800

    同步 过滤文件

---
+++ Src/Assets/Scripts/Framework/GOAP/Action/IAction.cs
@@ -0,0 +1,33 @@
+/** 
+----------------------------------
+ *Copyright(C) 2019 by IndieGame
+ *All rights reserved.
+ *FileName:     IAction
+ *Author:       @JCY
+ *Version:      0.0.1
+ *AuthorEmail:  jcyemail@qq.com
+ *UnityVersion：2019.1.0f2
+ *CreateTime:   2020/02/24 22:10:25
+ *Description:  IndieGame 
+ *History:
+ ----------------------------------
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
