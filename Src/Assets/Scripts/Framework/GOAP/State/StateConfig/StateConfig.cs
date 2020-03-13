dca3aa856137ea9ef32330a3c9db0ed2623f7d6e
倡印_PC_Win_CO <jcyemail@qq.com>
2020-03-13 18:44:51 +0800

    调整文件结构

--- Src/Assets/Scripts/Framework/GOAP/State/StateConfig/StateConfig.cs
+++ Src/Assets/Scripts/Framework/GOAP/State/StateConfig/StateConfig.cs
@@ -27,4 +27,26 @@ namespace Framework.GOAP
         [Rename("状态标签")] public T Tag;
         public List<StateAssembly> StateElement;
     }
+
+    /// <summary>
+    /// 存在进行判断
+    /// 不存在忽略判断
+    /// 应该专门写一个外部环境类保存时事计算参数 && 保存基础数值
+    /// </summary>
+    [Serializable]
+    public class StateAssembly
+    {
+        [Rename("元素标签")] public AIStateElementTag ElementTag;
+        [Rename("标志位")] public bool IsRight = true;
+
+        public StateAssembly()
+        {
+        }
+
+        public StateAssembly(AIStateElementTag elementTag, bool right)
+        {
+            this.ElementTag = elementTag;
+            this.IsRight = right;
+        }
+    }
 }
