7d50815cfc2e919bb3317009fbcdb37b7125117e
倡印_PC_Win_CO <jcyemail@qq.com>
2020-04-02 18:42:07 +0800

    地址寻址资源 调整

---
+++ Src/Assets/Scripts/Framework/Addressable/AddressableTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Framework.Addressable
+{
+    public class AddressableTest : MonoBehaviour
+    {
+        public AssetReference archerObject;
+
+        private void Start()
+        {
+//              archerObject .InstantiateAsync()
+        }
+    }
+}
