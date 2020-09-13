5214a610745ac4527585a89c02afff3d2d34cd47
JCY_PC_Master <jcyemail@qq.com>
2020-09-14 00:22:46 +0800

    Third Person Shooter E2: GameManager & InputController

---
+++ Part3/Third Person Shooter Tutorial/Assets/Scripts/InputController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public float Vertical;
+    public float Horizontal;
+    public Vector2 MouseInput;
+
+    private void Update()
+    {
+        Vertical = Input.GetAxis("Vertical");
+        Horizontal = Input.GetAxis("Horizontal");
+        MouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+    }
+}
---
+++ Part3/Third Person Shooter Tutorial/Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private InputController inputController;
+
+    private void Start()
+    {
+        inputController = GameManager.Instance.InputController;
+    }
+
+    private void Update()
+    {
+        Debug.Log($"Horizontal : {inputController.Horizontal}");
+        Debug.Log($"Mouse : {inputController.MouseInput}");
+    }
+}
