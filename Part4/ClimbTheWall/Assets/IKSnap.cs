7d56c8f7c0c2194cf54b66440c6f11ba093643f8
JCY_PC_Master <jcyemail@qq.com>
2020-09-20 23:22:59 +0800

    同步

---
+++ Part4/ClimbTheWall/Assets/IKSnap.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+
+public class IKSnap : MonoBehaviour
+{
+    public bool useIK;
+    public bool leftHandIK;
+    public bool rightHandIK;
+    private Animator anim;
+    public Vector3 leftHandPos;
+    public Vector3 rightHandPos;
+    public Quaternion leftHandRot;
+    public Quaternion rightHandRot;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit lHit;
+        RaycastHit rHit;
+        if (Physics.Raycast(transform.position + new Vector3(0.0f, 2.0f, 0.5f), -transform.up + new Vector3(-0.5f, 0.0f, 0.0f), out lHit, 1f))
+        {
+            leftHandIK = true;
+            leftHandPos = lHit.point;
+        }
+        else
+            leftHandIK = false;
+
+        if (Physics.Raycast(transform.position + new Vector3(0.0f, 2.0f, 0.5f), -transform.up + new Vector3(0.5f, 0.0f, 0.0f), out rHit, 1f))
+        {
+            rightHandIK = true;
+            rightHandPos = rHit.point;
+        }
+        else
+            rightHandIK = false;
+    }
+
+    private void Update()
+    {
+        Debug.DrawRay(transform.position + new Vector3(0.0f, 2.0f, 0.5f), -transform.up + new Vector3(-0.5f, 0.0f, 0.0f), Color.green);
+        Debug.DrawRay(transform.position + new Vector3(0.0f, 2.0f, 0.5f), -transform.up + new Vector3(0.5f, 0.0f, 0.0f), Color.green);
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        if (useIK)
+        {
+            if (leftHandIK)
+            {
+                anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1f);
+                anim.SetIKPosition(AvatarIKGoal.LeftHand, leftHandPos);
+            }
+
+            if (rightHandIK)
+            {
+                anim.SetIKPositionWeight(AvatarIKGoal.RightHand, 1f);
+                anim.SetIKPosition(AvatarIKGoal.RightHand, rightHandPos);
+            }
+        }
+    }
+}
