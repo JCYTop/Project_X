88866485dccce2269357abc571c44e4b0a7960a9
JCY_PC_Master <jcyemail@qq.com>
2020-11-17 00:25:36 +0800

    坐标系统 内容 练习还没有处理

--- Part6/GL/3d_panel.cpp
+++ Part6/GL/3d_panel.cpp
@@ -54,7 +54,7 @@
 //
 //	// build and compile our shader zprogram
 //	// ------------------------------------
-//	Shader ourShader("5.1.transform.vs", "5.1.transform.fs");
+//	Shader ourShader("6.1.coordinate_systems.vs", "6.1.coordinate_systems.fs");
 //
 //	// set up vertex data (and buffer(s)) and configure vertex attributes
 //	// ------------------------------------------------------------------
@@ -167,15 +167,26 @@
 //		glActiveTexture(GL_TEXTURE1);
 //		glBindTexture(GL_TEXTURE_2D, texture2);
 //
-//		// create transformations
-//		glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-//		transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-//		transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
-//
 //		// get matrix's uniform location and set matrix
 //		ourShader.use();
-//		unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
-//		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+//
+//		//坐标转换
+//		// create transformations
+//		glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+//		glm::mat4 view = glm::mat4(1.0f);
+//		glm::mat4 projection = glm::mat4(1.0f);
+//		model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+//		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+//		projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+//
+//		unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
+//		unsigned int viewLoc = glGetUniformLocation(ourShader.ID, "view");
+//		// pass them to the shaders (3 different ways)
+//		// Mark 传送到shader中的不同方式
+//		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+//		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+//		ourShader.setMat4("projection", projection);
+//		//
 //
 //		// render container
 //		glBindVertexArray(VAO);
